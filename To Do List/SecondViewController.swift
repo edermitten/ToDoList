78d66b9f081dae54c1e43bd7e96f37ee99b0da1b
Eder <edermitten@gmail.com>
2018-09-12 16:54:01 -0500

    first commit

--- To Do List/SecondViewController.swift
+++ To Do List/SecondViewController.swift
@@ -8,12 +8,46 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet var itemTextfield: UITextField!
+    
+    @IBAction func Add(_ sender: Any) {
+        
+        let itemsObject = UserDefaults.standard.object(forKey: "items")
+        
+        var items : [String]
+        
+        if let tempItems = itemsObject as? [String] {
+            
+            items = tempItems
+            
+            items.append(itemTextfield.text!)
+            
+        } else {
+            items = [itemTextfield.text!]
+        }
+        
+        UserDefaults.standard.set(items, forKey: "items")
+        itemTextfield.text = ""
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+        
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
